5ec7f02503b52f50812b8c33af1411a81f534f5a
Saara Virtanen <K1698@student.jamk.fi>
2016-02-17 11:00:59 +0200

    Initial commit

---
+++ T2/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace T2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void markkaButton_Click(object sender, RoutedEventArgs e)
+        {
+            string markkaString = alkuRaha.Text;
+            double markat = double.Parse(markkaString);
+            markat = markat * 5.94573;
+            muutettuRaha.Text = markat.ToString("0.00");
+        }
+
+        private void euroButton_Click(object sender, RoutedEventArgs e)
+        {
+            string euroString = alkuRaha.Text;
+            double eurot = double.Parse(euroString);
+            eurot = eurot * 0.17;
+            muutettuRaha.Text = eurot.ToString("0.00");
+        }
+    }
+}
